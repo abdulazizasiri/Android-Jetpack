b62ac0b31f8740949e73f443ce19789f56a57304
unknown <asiriaabdulaziz@gmail.com>
2021-05-01 01:44:02 +0300

    increment

--- App1_biggerNumber/app/src/main/java/com/example/whichisbigger/MainActivity.kt
+++ App1_biggerNumber/app/src/main/java/com/example/whichisbigger/MainActivity.kt
@@ -13,26 +13,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        var current : TextView = findViewById(R.id.number)
+
         var leftBtn: Button = findViewById(R.id.left)
         var rightBtn: Button = findViewById(R.id.right)
 
 
         leftBtn.setOnClickListener{
 
-            decrement(leftBtn, rightBtn)
+            decrement(current, true)
+        }
+
+        rightBtn.setOnClickListener{
+            decrement(current, false)
         }
 
 
     }
 
-    fun decrement(leftBtn: Button, rightBtn:Button) {
-        var num = leftBtn.text.toString().toInt()
+    fun decrement(curr: TextView, isDec: Boolean) {
+        var num = curr.text.toString().toInt()
         if (num == 0) {
             return
         }
-        num--;
-        leftBtn.text = "-:${num}"
-        rightBtn.text = "+:${num}"
+        if (isDec) {
+            num--;
+        } else {
+            num++
+        }
+        curr.text = "${num}"
     }
+
 }
