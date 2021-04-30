68962d7776c9bc99cd530b4b39879c529fa48f2b
unknown <asiriaabdulaziz@gmail.com>
2021-05-01 01:56:14 +0300

    done creating the bigger number app

--- App1_biggerNumber/app/src/main/java/com/example/whichisbigger/MainActivity.kt
+++ App1_biggerNumber/app/src/main/java/com/example/whichisbigger/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.whichisbigger
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,25 +20,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var current : TextView = findViewById(R.id.number)
 
+        var submit = findViewById<Button>(R.id.check)
         var leftBtn: Button = findViewById(R.id.left)
         var rightBtn: Button = findViewById(R.id.right)
 
 
         leftBtn.setOnClickListener{
 
-            decrement(current, true)
+            changeValue(current, true)
         }
 
         rightBtn.setOnClickListener{
-            decrement(current, false)
+            changeValue(current, false)
+        }
+
+        submit.setOnClickListener{
+            checkValue(current)
         }
 
 
     }
-
-    fun decrement(curr: TextView, isDec: Boolean) {
+    @SuppressLint("ResourceAsColor")
+    fun checkValue(current: TextView){
+        var currentValue = 21 /// Vurrently. I am bored, I want to move on.
+        if (current.text.toString().toInt() == currentValue) {
+            findViewById<ConstraintLayout>(R.id.bg).setBackgroundColor(R.color.teal_700)
+        }
+    }
+    fun changeValue(curr: TextView, isDec: Boolean) {
         var num = curr.text.toString().toInt()
-        if (num == 0) {
+        if (num == 0 && isDec) {
             return
         }
         if (isDec) {
